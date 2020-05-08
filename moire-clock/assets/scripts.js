1256ed72519d52cd94f90001a8dc522178f74fea
Calen Chung <calenchung@gmail.com>
2020-05-08 01:00:17 -0400

    moire

---
+++ moire-clock/assets/scripts.js
@@ -0,0 +1,485 @@
+
+    var circle0 = new Path.Circle(view.center, 3);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 6);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 9);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 12);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 15);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 18);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 21);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 24);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 27);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 30);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 33);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 36);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 39);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 42);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 45);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 48);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 51);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 54);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 57);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 60);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 63);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 66);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 69);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 72);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 75);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 78);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 81);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 84);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 87);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 90);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 93);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 96);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 99);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 102);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 105);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 108);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 111);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 114);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 117);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 120);
+    circle0.strokeColor = 'black';
+    var circle0 = new Path.Circle(view.center, 123);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 126);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 129);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 132);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 135);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 138);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 141);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 144);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 147);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 150);
+    circle0.strokeColor = 'black';
+    var circle0 = new Path.Circle(view.center, 153);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 156);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 159);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 162);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 165);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 168);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 171);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 174);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 177);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 180);
+    circle0.strokeColor = 'black';
+    var circle0 = new Path.Circle(view.center, 183);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 186);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 189);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 192);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 195);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 198);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 201);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 204);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 207);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 210);
+    circle0.strokeColor = 'black';
+
+
+
+
+    // ************************SECONDS******************************
+
+
+
+
+
+    var circle1 = new Path.Circle(view.center, 3);
+    circle1.strokeColor = 'black';
+    circle1.strokeWidth = '1';
+    var circle2 = new Path.Circle(view.center, 6);
+    circle2.strokeColor = 'black';
+    circle2.strokeWidth = '1';
+    var circle3 = new Path.Circle(view.center, 9);
+    circle3.strokeColor = 'black';
+    circle3.strokeWidth = '1';
+    var circle4 = new Path.Circle(view.center, 12);
+    circle4.strokeColor = 'black';
+    circle4.strokeWidth = '1';
+    var circle5 = new Path.Circle(view.center, 15);
+    circle5.strokeColor = 'black';
+    circle5.strokeWidth = '1';
+    var circle6 = new Path.Circle(view.center, 18);
+    circle6.strokeColor = 'black';
+    circle6.strokeWidth = '1';
+    var circle7 = new Path.Circle(view.center, 21);
+    circle7.strokeColor = 'black';
+    circle7.strokeWidth = '1';
+    var circle8 = new Path.Circle(view.center, 24);
+    circle8.strokeColor = 'black';
+    circle8.strokeWidth = '1';
+    var circle9 = new Path.Circle(view.center, 27);
+    circle9.strokeColor = 'black';
+    circle9.strokeWidth = '1';
+    var circle10 = new Path.Circle(view.center, 30);
+    circle10.strokeColor = 'black';
+    circle10.strokeWidth = '1';
+    var circle11 = new Path.Circle(view.center, 33);
+    circle11.strokeColor = 'black';
+    circle11.strokeWidth = '1';
+    var circle12 = new Path.Circle(view.center, 36);
+    circle12.strokeColor = 'black';
+    circle12.strokeWidth = '1';
+    var circle13 = new Path.Circle(view.center, 39);
+    circle13.strokeColor = 'black';
+    circle13.strokeWidth = '1';
+    var circle14 = new Path.Circle(view.center, 42);
+    circle14.strokeColor = 'black';
+    circle14.strokeWidth = '1';
+    var circle15 = new Path.Circle(view.center, 45);
+    circle15.strokeColor = 'black';
+    circle15.strokeWidth = '1';
+    var circle16 = new Path.Circle(view.center, 48);
+    circle16.strokeColor = 'black';
+    circle16.strokeWidth = '1';
+    var circle17 = new Path.Circle(view.center, 51);
+    circle17.strokeColor = 'black';
+    circle17.strokeWidth = '1';
+    var circle18 = new Path.Circle(view.center, 54);
+    circle18.strokeColor = 'black';
+    circle18.strokeWidth = '1';
+    var circle19 = new Path.Circle(view.center, 57);
+    circle19.strokeColor = 'black';
+    circle19.strokeWidth = '1';
+    var circle20 = new Path.Circle(view.center, 60);
+    circle20.strokeColor = 'black';
+    circle20.strokeWidth = '1';
+    var circle21 = new Path.Circle(view.center, 63);
+    circle21.strokeColor = 'black';
+    circle21.strokeWidth = '1';
+    var circle22 = new Path.Circle(view.center, 66);
+    circle22.strokeColor = 'black';
+    circle22.strokeWidth = '1';
+    var circle23 = new Path.Circle(view.center, 69);
+    circle23.strokeColor = 'black';
+    circle23.strokeWidth = '1';
+    var circle24 = new Path.Circle(view.center, 72);
+    circle24.strokeColor = 'black';
+    circle24.strokeWidth = '1';
+    var circle25 = new Path.Circle(view.center, 75);
+    circle25.strokeColor = 'black';
+    circle25.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 78);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 81);
+    circle0.strokeColor = 'black';
+    circle0.strokeWidth = '1';
+    var circle26 = new Path.Circle(view.center, 84);
+    circle26.strokeColor = 'black';
+    circle26.strokeWidth = '1';
+    var circle27 = new Path.Circle(view.center, 87);
+    circle27.strokeColor = 'black';
+    circle27.strokeWidth = '1';
+    var circle28 = new Path.Circle(view.center, 90);
+    circle28.strokeColor = 'black';
+    circle28.strokeWidth = '1';
+    var circle29 = new Path.Circle(view.center, 93);
+    circle29.strokeColor = 'black';
+    circle29.strokeWidth = '1';
+    var circle30 = new Path.Circle(view.center, 96);
+    circle30.strokeColor = 'black';
+    circle30.strokeWidth = '1';
+    var circle31 = new Path.Circle(view.center, 99);
+    circle31.strokeColor = 'black';
+    circle31.strokeWidth = '1';
+    var circle32 = new Path.Circle(view.center, 102);
+    circle32.strokeColor = 'black';
+    circle32.strokeWidth = '1';
+    var circle33 = new Path.Circle(view.center, 105);
+    circle33.strokeColor = 'black';
+    circle33.strokeWidth = '1';
+    var circle34 = new Path.Circle(view.center, 108);
+    circle34.strokeColor = 'black';
+    circle34.strokeWidth = '1';
+    var circle35 = new Path.Circle(view.center, 111);
+    circle35.strokeColor = 'black';
+    circle35.strokeWidth = '1';
+    var circle36 = new Path.Circle(view.center, 114);
+    circle36.strokeColor = 'black';
+    circle36.strokeWidth = '1';
+    var circle37 = new Path.Circle(view.center, 117);
+    circle37.strokeColor = 'black';
+    circle37.strokeWidth = '1';
+    var circle38 = new Path.Circle(view.center, 120);
+    circle38.strokeColor = 'black';
+    circle38.strokeWidth = '1';
+    var circle0 = new Path.Circle(view.center, 123);
+    circle39.strokeColor = 'black';
+    circle39.strokeWidth = '1';
+    var circle40 = new Path.Circle(view.center, 126);
+    circle40.strokeColor = 'black';
+    circle40.strokeWidth = '1';
+    var circle41 = new Path.Circle(view.center, 129);
+    circle41.strokeColor = 'black';
+    circle41.strokeWidth = '1';
+    var circle42 = new Path.Circle(view.center, 132);
+    circle42.strokeColor = 'black';
+    circle42.strokeWidth = '1';
+    var circle43 = new Path.Circle(view.center, 135);
+    circle43.strokeColor = 'black';
+    circle43.strokeWidth = '1';
+    var circle44 = new Path.Circle(view.center, 138);
+    circle44.strokeColor = 'black';
+    circle44.strokeWidth = '1';
+    var circle45 = new Path.Circle(view.center, 141);
+    circle45.strokeColor = 'black';
+    circle45.strokeWidth = '1';
+    var circle46 = new Path.Circle(view.center, 144);
+    circle46.strokeColor = 'black';
+    circle46.strokeWidth = '1';
+    var circle47 = new Path.Circle(view.center, 147);
+    circle47.strokeColor = 'black';
+    circle47.strokeWidth = '1';
+    var circle48 = new Path.Circle(view.center, 150);
+    circle48.strokeColor = 'black';
+    circle48.strokeWidth = '1';
+    var circle49 = new Path.Circle(view.center, 153);
+    circle49.strokeColor = 'black';
+    circle49.strokeWidth = '1';
+    var circle50 = new Path.Circle(view.center, 156);
+    circle50.strokeColor = 'black';
+    circle50.strokeWidth = '1';
+    var circle51 = new Path.Circle(view.center, 159);
+    circle51.strokeColor = 'black';
+    circle51.strokeWidth = '1';
+    var circle52 = new Path.Circle(view.center, 162);
+    circle52.strokeColor = 'black';
+    circle52.strokeWidth = '1';
+    var circle53 = new Path.Circle(view.center, 165);
+    circle53.strokeColor = 'black';
+    circle53.strokeWidth = '1';
+    var circle53 = new Path.Circle(view.center, 168);
+    circle53.strokeColor = 'black';
+    circle53.strokeWidth = '1';
+    var circle54 = new Path.Circle(view.center, 171);
+    circle54.strokeColor = 'black';
+    circle54.strokeWidth = '1';
+    var circle54 = new Path.Circle(view.center, 174);
+    circle54.strokeColor = 'black';
+    circle54.strokeWidth = '1';
+    var circle55 = new Path.Circle(view.center, 177);
+    circle55.strokeColor = 'black';
+    circle55.strokeWidth = '1';
+
+
+
+
+
+
+
+// ******************************************************
+
+
+
+    date = new Date();
+    seconds = date.getSeconds()
+
+      var group = new Group();
+      group.addChild(circle1)
+      group.addChild(circle2)
+      group.addChild(circle3)
+      group.addChild(circle4)
+      group.addChild(circle5)
+      group.addChild(circle6)
+      group.addChild(circle7)
+      group.addChild(circle8)
+      group.addChild(circle9)
+      group.addChild(circle10)
+      group.addChild(circle11)
+      group.addChild(circle12)
+      group.addChild(circle13)
+      group.addChild(circle14)
+      group.addChild(circle15)
+      group.addChild(circle16)
+      group.addChild(circle17)
+      group.addChild(circle18)
+      group.addChild(circle19)
+      group.addChild(circle20)
+      group.addChild(circle21)
+      group.addChild(circle22)
+      group.addChild(circle23)
+      group.addChild(circle24)
+      group.addChild(circle25)
+      group.addChild(circle26)
+      group.addChild(circle27)
+      group.addChild(circle28)
+      group.addChild(circle29)
+      group.addChild(circle30)
+      group.addChild(circle31)
+      group.addChild(circle32)
+      group.addChild(circle33)
+      group.addChild(circle34)
+      group.addChild(circle35)
+      group.addChild(circle36)
+      group.addChild(circle37)
+      group.addChild(circle38)
+      group.addChild(circle39)
+      group.addChild(circle40)
+      group.addChild(circle41)
+      group.addChild(circle42)
+      group.addChild(circle43)
+      group.addChild(circle44)
+      group.addChild(circle45)
+      group.addChild(circle46)
+      group.addChild(circle47)
+      group.addChild(circle48)
+      group.addChild(circle49)
+      group.addChild(circle50)
+      group.addChild(circle51)
+      group.addChild(circle52)
+      group.addChild(circle53)
+      group.addChild(circle54)
+      group.addChild(circle55)
+
+      group.postion = view.center;
+      group.rotate(seconds);
+
+      group.position.x -=100;
+
+
+    setInterval(function() {
+        //update date variables
+        date = new Date();
+        seconds = date.getSeconds()
+        // minutes = date.getMinutes()
+
+        group.rotate(6, view.center);
+
+        //call if seconds reset (1 minute)
+        if(seconds == 0){
+            circleM.rotate(6);
+        }
+
+    }, 1000)
